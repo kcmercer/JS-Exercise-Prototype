f553f6d1190a3d7b427231647dac6f25e225ac99
Kenneth Mercer <kmercer5439@gmail.com>
2021-11-10 13:28:14 -0600

    solved tasks 3 and 4

--- index.js
+++ index.js
@@ -39,14 +39,39 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.stomach = [];
+  this.name = name,
+  this.age = age
 }
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
+const neo = new Person('Neo', 20);
+const kyler = new Person('Kyler', 25);
+const ben = new Person('Bennett', 28);
 
+console.log(neo.toString())
+
+ben.eat('food');
+
+console.log('task 1 -', ben.stomach);
+
+ben.poop();
+
+console.log('task 1 -', ben.stomach);
 
 
 /*
@@ -63,9 +88,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0,
+  this.odometer = 0
 }
+
+Car.prototype.fill = function(gallons) {
+  return this.tank = gallons;
+}
+
+const impala = new Car('Impala', 25);
+
+console.log('task 2 -', impala);
+
+impala.fill(20);
+
+console.log('task 2 -', impala.tank);
+
 
 
 /*
@@ -75,18 +116,32 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this)
+  this.name = name,
+  this.age = age,
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = 
+Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
+
+const roland = new Baby('Roland', 1, 'Donald Duck');
+
+roland.play();
+
+console.log('task 3 -', roland)
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. If nothing else calls for it, this defualts to the window
+  2. If within objects with methods this will apply only inside of that object (whatever is to the left of the .)
+  3. If we explicitly tell this what we want it to refer to using .apply, .bind or .call this will apply to only that.
+  4. If we use it in a constructor function, when we pass in the arguement name, this.name will refer to whatever is input.
 */
 
 
